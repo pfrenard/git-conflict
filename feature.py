0053f364df66b6b14a0bf979539980f3fe5ae985
Pierre-Francois RENARD <pfrenard@gmail.com>
2020-01-21 14:12:04 +0100

    randint error

--- feature.py
+++ feature.py
@@ -5,7 +5,7 @@ def feature_a():
     feature A
 
     """
-    return [x for x in range(0,random.randint(10000)) ]
+    return [x for x in range(0,random.randint(0,10000)) ]
 
 def feature_b():
     """
